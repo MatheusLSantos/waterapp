8de3c7d3dded04cc493c829301ef4d1426b0e2c0
MatheusLSantos <matheus2737p@gmail.com>
2023-11-07 23:56:27 -0300

    add water intake history

--- lib/water_intake_history.dart
+++ lib/water_intake_history.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class waterIntakeHistory extends StatefulWidget {
-  const waterIntakeHistory({super.key});
+class WaterIntakeHistory extends StatefulWidget {
+  const WaterIntakeHistory({super.key});
 
   @override
-  State<waterIntakeHistory> createState() => _waterIntakeHistoryState();
+  State<WaterIntakeHistory> createState() => _WaterIntakeHistoryState();
 }
 
-class _waterIntakeHistoryState extends State<waterIntakeHistory> {
+class _WaterIntakeHistoryState extends State<WaterIntakeHistory> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,12 +19,13 @@ class _waterIntakeHistoryState extends State<waterIntakeHistory> {
         itemCount: 3,
         itemBuilder: (context, index) {
           return Container(
+            padding: EdgeInsets.fromLTRB(24, 8, 24, 0),
             child: Card(
               color: Theme.of(context).colorScheme.tertiaryContainer,
               elevation: 0,
               child: ListTile(
-                title: Text('1500ml / 2000ml'),
-                trailing: Text("06/11/2023 12:06"),
+                title: Text('1500ml / 2000ml', style: TextStyle(fontWeight: FontWeight.bold),),
+                trailing: Text("06/11/2023 12:06", style: TextStyle(fontSize: 16),),
               ),
             ),
           );
