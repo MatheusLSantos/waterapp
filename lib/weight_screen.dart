2112229529425d1e01be99b55a9d3cd049fc2d1b
MatheusLSantos <matheus2737p@gmail.com>
2023-11-17 17:22:12 -0300

    add restrictions in weight screen

--- lib/weight_screen.dart
+++ lib/weight_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Importe o pacote necessário
 import 'package:provider/provider.dart';
 import 'home_screen.dart';
 import 'package:lottie/lottie.dart';
 import 'configs/app_settings.dart';
 
 class WeightScreen extends StatelessWidget {
+  final TextEditingController _weightController = TextEditingController();
+  static const MAXWEIGHT = 600;
+  static const MLWEIGHT = 35;
 
-  TextEditingController _weightController = TextEditingController();
+  String? _validateInput(String value) {
+    if (value.isEmpty) {
+      return 'Campo obrigatório';
+    }
+
+    try {
+      double weight = double.parse(value);
+      if (weight <= 2 || weight > MAXWEIGHT) {
+        return 'O peso deve estar entre 3 e $MAXWEIGHT kg';
+      }
+    } catch (e) {
+      return 'Digite um valor de peso válido';
+    }
+
+    return null; // A entrada é válida
+  }
+
+  void _setIntakeGoal(BuildContext context, double weight) async {
+    double intakeGoal = weight * MLWEIGHT;
+    context.read<AppSettings>().setIntakeGoal(intakeGoal);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,21 +66,37 @@ class WeightScreen extends StatelessWidget {
                     ),
                     SizedBox(height: 20),
                     TextField(
-                      maxLength: 4,
-                      keyboardType: TextInputType.number,
+                      maxLength: 5,
+                      keyboardType: TextInputType.numberWithOptions(decimal: true),
                       controller: _weightController,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d{0,1}$')),
+                      ],
                       decoration: InputDecoration(
-                        labelText: 'Peso (kg)',
+                        labelText: 'Exemplo: 74.2',
                         border: UnderlineInputBorder(),
+                        errorText: _validateInput(_weightController.text),
                       ),
                     ),
                     SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
-                        // Lógica para salvar o peso do usuário
-                        double value = double.tryParse(_weightController.text) ?? 0.0;
-                        context.read<AppSettings>().setWeight(value);
-                        Navigator.push(context, MaterialPageRoute(builder: (context) =>HomeScreen()));
+                        String? validationError = _validateInput(_weightController.text);
+                        if (validationError == null) {
+                          // A entrada é válida, salvar o peso do usuário
+                          double value = double.parse(_weightController.text);
+                          context.read<AppSettings>().setWeight(value);
+                          _setIntakeGoal(context, value);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
+                        } else {
+                          // A entrada é inválida, exibir uma mensagem de erro ou tomar outra ação necessária
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(validationError),
+                              backgroundColor: Theme.of(context).colorScheme.error,
+                            ),
+                          );
+                        }
                       },
                       child: Text('Salvar'),
                     ),
