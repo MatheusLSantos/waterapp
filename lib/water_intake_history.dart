ad71936814db6cc3ba7b5e085dc8fc2e98d0a174
MatheusLSantos <matheus2737p@gmail.com>
2023-11-20 01:33:53 -0300

    update water intake history

--- lib/water_intake_history.dart
+++ lib/water_intake_history.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:waterproject3/business_logic/waterIntakeByDiary.dart';
+import 'package:waterproject3/models/water_intake_model.dart';
+import 'package:waterproject3/models/diary_model.dart';
+import 'package:waterproject3/repositories/diary_repository.dart';
+import 'package:waterproject3/repositories/water_intake_repository.dart';
 
 class WaterIntakeHistory extends StatefulWidget {
   const WaterIntakeHistory({Key? key});
@@ -9,7 +15,8 @@ class WaterIntakeHistory extends StatefulWidget {
 }
 
 class _WaterIntakeHistoryState extends State<WaterIntakeHistory> {
-  Future<void> _showDeleteConfirmationDialog() async {
+  
+  Future<void> _showDeleteConfirmationDialog(BuildContext context, int waterIntakeId) async {
     return showDialog<void>(
       context: context,
       builder: (BuildContext context) {
@@ -25,7 +32,8 @@ class _WaterIntakeHistoryState extends State<WaterIntakeHistory> {
             TextButton(
               onPressed: () {
                 // Lógica para excluir do banco de dados aqui
-
+                context.read<WaterIntakeRepository>().deleteWaterIntake(waterIntakeId);
+                setState(() {});
                 // Fechar o pop-up
                 Navigator.of(context).pop();
               },
@@ -37,8 +45,20 @@ class _WaterIntakeHistoryState extends State<WaterIntakeHistory> {
     );
   }
 
+  String formatDateTime(DateTime time) {
+    return '${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')} - ${time.day.toString().padLeft(2, '0')}-${time.month.toString().padLeft(2, '0')}-${time.year}';
+  }
+  
+  Future<List<WaterIntake>> getWaterIntakes(Future<Diary> futureDiary ) async {
+    Diary diary = await futureDiary;
+    
+    return await context.read<DiaryRepository>().waterIntakes(diary.id);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Future<Diary> _diary = WaterIntakeByDiary.getDiary(context);
+    getWaterIntakes(_diary);
     return Stack(
       children: [
         Container(
@@ -57,32 +77,43 @@ class _WaterIntakeHistoryState extends State<WaterIntakeHistory> {
             backgroundColor: Colors.transparent,
             title: Text("Histórico de ingestão de água"),
           ),
-          body: ListView.builder(
-            itemCount: 3,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: EdgeInsets.fromLTRB(24, 8, 24, 0),
-                child: Card(
-                  color: Theme.of(context).colorScheme.tertiaryContainer,
-                  elevation: 0,
-                  child: ListTile(
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('1500ml / 2000ml', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () {
-                            _showDeleteConfirmationDialog();
-                          },
+          body: FutureBuilder(
+            future: getWaterIntakes(_diary),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  reverse: true,
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.fromLTRB(24, 8, 24, 0),
+                      child: Card(
+                        color: Theme.of(context).colorScheme.tertiaryContainer,
+                        elevation: 0,
+                        child: ListTile(
+                          title: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('${snapshot.data![index].waterIntakeVolume.round()}ml', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
+                              Text('${formatDateTime(snapshot.data![index].createdAt)}', style: TextStyle(fontSize: 12)),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  _showDeleteConfirmationDialog(context, snapshot.data![index].id);
+                                },
+                              ),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
-                    subtitle: Text("06/11/2023 12:06", style: TextStyle(fontSize: 12)),
-                  ),
-                ),
-              );
-            },
+                      ),
+                    );
+                  },
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            }
           ),
         ),
       ],
