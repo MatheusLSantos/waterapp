74f3ef3923ae693aa6154a5ec847498a9f58b89c
MatheusLSantos <matheus2737p@gmail.com>
2023-11-19 21:02:43 -0300

    persist theme in shared preferences database

--- lib/components/switch_theme_toggle.dart
+++ lib/components/switch_theme_toggle.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:waterproject3/theme/color_schemes.g.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SwitchThemeToggle extends StatelessWidget {
+class SwitchThemeToggle extends StatefulWidget {
+  @override
+  State<SwitchThemeToggle> createState() => _SwitchThemeToggleState();
+}
+
+class _SwitchThemeToggleState extends State<SwitchThemeToggle> {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
@@ -26,8 +32,23 @@ class ThemeProvider with ChangeNotifier {
 
   bool get isDarkMode => _isDarkMode;
 
-  void toggleTheme() {
-    _isDarkMode = !_isDarkMode;
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  void _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool('isDarkMode') ?? false;
     notifyListeners();
   }
+
+  void toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+
+    // Save the theme preference to shared preferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDarkMode', _isDarkMode);
+  }
+
 }
