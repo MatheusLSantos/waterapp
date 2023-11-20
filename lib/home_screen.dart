285ecb46d1b0d1b235a16fc1456427d2699cb29f
MatheusLSantos <matheus2737p@gmail.com>
2023-11-20 01:39:37 -0300

    update home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:waterproject3/business_logic/waterIntakeByDiary.dart';
 import 'package:waterproject3/configs/app_settings.dart';
 import 'package:waterproject3/models/diary_model.dart';
 import 'package:waterproject3/models/water_intake_model.dart';
-import 'package:waterproject3/repositories/diary_repository.dart';
 import 'package:waterproject3/repositories/water_intake_repository.dart';
 import 'settings_screen.dart';
 import 'package:lottie/lottie.dart';
 import 'water_intake_history.dart';
 import 'components/water_selection.dart';
 import 'components/random_tip_generator.dart';
-import 'package:collection/collection.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
       builder: (BuildContext context) {
         return WaterSeleciton(
           onButtonPressed: (int buttonIndex) async {
-            Diary diary = await _getDiary();
+            Diary diary = await WaterIntakeByDiary.getDiary(context);
 
             WaterIntake waterIntake = WaterIntake(waterIntakeVolume: buttonIndex.toDouble(), createdAt: DateTime.now(), diary: diary.id);
             await context.read<WaterIntakeRepository>().insertWaterIntake(waterIntake);
@@ -49,34 +48,6 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _tipText = RandomTipGenerator.getRandomTip();
     });
-  }
-
-  Future<double> _getTotalIntake() async {
-    Diary diary = await _getDiary();
-
-    double totalIntake = 0.0;
-
-    List<WaterIntake> waterIntakes = await context.read<DiaryRepository>().waterIntakes(diary.id);
-    waterIntakes.forEach((waterIntake) {
-      totalIntake += waterIntake.waterIntakeVolume;
-    });
-
-    return totalIntake;
-  }
-
-  Future<Diary> _getDiary() async {
-    List<Diary> diarys = await context.read<DiaryRepository>().diarys();
-    Diary? diary = await diarys.firstWhereOrNull((element) => element.day == DateTime.now().day && element.month == DateTime.now().month && element.year == DateTime.now().year);
-
-    if (diary == null) {
-      Diary new_diary = Diary(year: DateTime.now().year, month: DateTime.now().month, day: DateTime.now().day);
-      int diary_id = await context.read<DiaryRepository>().insertDiary(new_diary);
-      Diary created_diary = await context.read<DiaryRepository>().getDiary(diary_id);
-      return created_diary;
-    }
-    else {
-      return diary;
-    }
   }
 
   @override
@@ -172,7 +143,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                           ),
                           FutureBuilder<double>(
-                              future: _getTotalIntake(),
+                              future: WaterIntakeByDiary.getTotalIntake(context),
                               builder: (BuildContext context, AsyncSnapshot<double> snapshot){
                                 if (snapshot.hasData) {
                                   return Text("${snapshot.data!.round()}ml", style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),);
