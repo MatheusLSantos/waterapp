c575544e54c32e522a6167ff34a451e99a3174cf
MatheusLSantos <matheus2737p@gmail.com>
2023-11-21 14:38:45 -0300

    create first time function in spash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
+import 'package:waterproject3/name_screen.dart';
+import 'configs/app_settings.dart';
+import 'home_screen.dart';
 
 
 class SplashScreen extends StatefulWidget {
@@ -11,9 +16,44 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 4), () {
-      Navigator.pushReplacementNamed(context, '/home');
-    });
+    _initApp();
+  }
+
+  Future<void> _initApp() async {
+    // Aguarde a inicialização do AppSettings
+    await _initAppSettings();
+
+    // Simule o carregamento da tela de splash
+    await Future.delayed(Duration(seconds: 3));
+
+    // Verifique se o usuário já viu a tela
+    bool _seen = await _getSeenStatus();
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } else {
+      await _setSeenStatus(true);
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => NameScreen()),
+      );
+    }
+  }
+
+  Future<void> _initAppSettings() async {
+    // Inicialize o AppSettings aqui
+    await Provider.of<AppSettings>(context, listen: false).startPreferences();
+  }
+
+  Future<bool> _getSeenStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('seen') ?? false;
+  }
+
+  Future<void> _setSeenStatus(bool seen) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('seen', seen);
   }
 
   @override
@@ -25,3 +65,4 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
+
